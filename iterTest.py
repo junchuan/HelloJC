13702528a938002aaa404786dbe6d3b83d59f765
junchuan <fanjunchuan@yahoo.com>
2014-12-30 14:00:38 -0600

    added first file

---
+++ iterTest.py
@@ -0,0 +1,22 @@
+def integers():
+    i=1
+    while True:
+        yield i
+        i=i+1
+
+def squares():
+    for i in integers():
+        yield i*i
+
+
+def taken(n,seq):
+    seq=iter(seq)
+    result=[]
+    try:
+        for i in range(n):
+            result.append(seq.next())
+    except StopIteration:
+        pass
+    return result
+
+
